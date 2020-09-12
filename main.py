dbf4f6ee908d42e1762b512a8df4aa91c7373375
Andy Harless <a23@harless.us>
2020-09-12 13:55:22 -0400

    add new learning parameters

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from math import exp
 import random
 
 # Control parameters
-n_episodes = 500000
+n_episodes = 250000
 nruns = 1
 medsub = nruns // 2
 
 # Learning parameters
+beta=.7
+c1=.02
+c2=1
 alpha=.7
 gamma=.5
 a = -.005
@@ -28,6 +31,9 @@ local_seed = 80
 start = 3
 
 print("\n\nBegin Taxi-v3 learning...")
+print("Parameters:")
+print("beta={}, Q_weight={}, recency_weight={}, start={}, decay={}".format(beta, c1, c2, a, b))
+print("learning_rate={}, discount_rate={}, min_stochasticity={}".format(alpha, gamma, eps_min))
 
 # Multiple sample runs (but in this case only the best one)
 for i in range(start, start+nruns):
@@ -42,15 +48,15 @@ for i in range(start, start+nruns):
     env.action_space.seed(1000000*i+local_seed)
     
     # Run the learning problem
-    agent = Agent(alpha=alpha, gamma=gamma, get_epsilon=epfunc)
-    avg_rewards, best_avg_reward = interact(env, agent, n_episodes, show_progress=10000)
+    agent = Agent(alpha=alpha, gamma=gamma, get_epsilon=epfunc, c1=c1, c2=c2, beta=beta)
+    avg_rewards, best_avg_reward = interact(env, agent, n_episodes, show_progress=10000, endline='\n')
     best_avg_rewards.append(best_avg_reward)
     
     # Monitor results after each run
     print("\rRun {}/{}, average so far={}".format(i, nruns, 
-                                        sum(best_avg_rewards)/len(best_avg_rewards)), end="")
+                                        sum(best_avg_rewards)/len(best_avg_rewards)), end="\n")
     
 print('\nLocal seed: ', local_seed)
 print('Average: ', sum(best_avg_rewards)/len(best_avg_rewards))
 print('Median: ', sorted(best_avg_rewards)[medsub])
-np.array(sorted(best_avg_rewards))
+print(np.array(sorted(best_avg_rewards)))
